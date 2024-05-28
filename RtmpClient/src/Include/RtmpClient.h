4552c5a81afd7035a54cbf8e4b166539dc83b94d
fengweiyu <984441750@qq.com>
2024-05-28 11:52:35 +0800

    [CHG]rtmp client 修改

--- RtmpClient/src/Include/RtmpClient.h
+++ RtmpClient/src/Include/RtmpClient.h
@@ -12,11 +12,7 @@
 #ifndef RTMP_CLIENT_H
 #define RTMP_CLIENT_H 
 
-#include "XStreamParser.h"
 #include "RtmpSession.h"
-#include "XNet.h"
-#include "XNetClient.h"
-#include "STDStream.h"
 ;
 
 /*****************************************************************************
@@ -26,41 +22,24 @@
 * -----------------------------------------------
 * 2023/09/21	  V1.0.0		 Yu Weifeng 	  Created
 ******************************************************************************/
-class RtmpClient  : public CXNetClient
+class RtmpClient  
 {
 public:
     RtmpClient();
-    RtmpClient(int i_iPlayOrPublish,char * i_strURL);
+    RtmpClient(int i_iPlayOrPublish,char * i_strURL,T_RtmpCb *i_ptRtmpCb);
     virtual ~RtmpClient();
     
 	
-    int Start(int i_iPlayOrPublish,char * i_strURL);
+    int Start(int i_iPlayOrPublish,char * i_strURL,T_RtmpCb *i_ptRtmpCb);
     int Stop(int err);
-    int Pushing(FRAME_INFO * i_pFrame);
+    int Pushing(T_RtmpMediaInfo *i_ptRtmpMediaInfo,unsigned char * i_pbFrameData,int i_iFrameLen,char * i_strPlayPath = NULL);
     
-    static int ConnectServer(void *i_pIoHandle,char * i_strIP,unsigned short i_wPort);
-    static int SendDatas(void *i_pIoHandle,char * i_acSendBuf,int i_iSendLen);
-    static int PlayVideoData(T_RtmpMediaInfo *i_ptRtmpMediaInfo,char * i_acDataBuf,int i_iDataLen,void *i_pIoHandle);
-    static int PlayAudioData(T_RtmpMediaInfo *i_ptRtmpMediaInfo,char * i_acDataBuf,int i_iDataLen,void *i_pIoHandle);
-    static int PlayScriptData(char *i_strStreamName,unsigned int i_dwTimestamp,char * i_acDataBuf,int i_iDataLen);
-    
-    static long GetRandom();
-
-    int ConnectToServer(char * i_strIP,unsigned short i_wPort);
-    
-protected:
-    int OnMsg(PXMSG msg);
-    int OnConnect(int nResult);
-    int OnDisConnect(int nError);
-    void OnRecvData(const char* pData, int nDataLen);
+    int DoConnect();
+    int DoCycle(char *i_pcData,int i_iDataLen);
     
 private:
     
-    int SetTimer(int i_iTimeMs);
-    static FRAME_INFO* RtmpFrameToFrameInfo(T_RtmpMediaInfo * i_ptRtmpMediaInfo,unsigned char *i_pbFrameData,int i_iDataLen);
-    
 	RtmpSession *   m_pRtmpSession;
-    XHANDLE m_iTimer;
     int m_iStarter;
     int m_iSendResed;
     int m_iSendResTime;
